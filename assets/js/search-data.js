9c8e9cf0da63c8a1594dd32115268d1a9ec12847
DylanTao <31508165+DylanTao@users.noreply.github.com>
2025-04-04 23:22:04 +0000

    Deploying to gh-pages from @ DylanTao/dylantao.github.io@c4038f93ffa51f6e085adb0b3ea37069b74cca7a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,18 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "post-a-post-with-plotly-js",
-        
-          title: "a post with plotly.js",
-        
-        description: "this is what included plotly.js code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2025/plotly/";
-          
-        },
-      },{id: "post-a-gallery-of-random-gen-ai-images",
+        },{id: "post-a-gallery-of-random-gen-ai-images",
         
           title: "a gallery of random gen-ai images",
         
