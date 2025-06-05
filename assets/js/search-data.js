ddaef1cd3e4d3fd8e82e35042945a4d681a11692
DylanTao <31508165+DylanTao@users.noreply.github.com>
2025-06-05 07:55:13 +0000

    Deploying to gh-pages from @ DylanTao/dylantao.github.io@c2527022698ae1955af46d294352621deba3c3c0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -113,13 +113,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/siruitao", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
