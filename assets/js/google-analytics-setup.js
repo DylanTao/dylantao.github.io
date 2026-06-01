abbf1c2592b79f9da891c55d9b0b69de58577b52
DylanTao <31508165+DylanTao@users.noreply.github.com>
2026-06-01 22:36:37 +0000

    Deploying to gh-pages from @ DylanTao/dylantao.github.io@e06d64e3b7209b1dfa3cedc5284f8b97844edb29 🚀

--- assets/js/google-analytics-setup.js
+++
@@ -1,8 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-window.gtag =
-  window.gtag ||
-  function () {
-    window.dataLayer.push(arguments);
-  };
-window.gtag("js", new Date());
-window.gtag("config", "G-L4SQ3C88KH");
