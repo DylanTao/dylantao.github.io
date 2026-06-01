0feb9ceb448f5c303568378ce438ecf038ac4148
DylanTao <31508165+DylanTao@users.noreply.github.com>
2026-06-01 04:38:58 +0000

    Deploying to gh-pages from @ DylanTao/dylantao.github.io@b6d6f24588ccb9660c605c94c9e20e8cff25609c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -226,7 +226,7 @@ ninja.data = [
           },
         },{id: "project-hci-spooder-man",
         title: "HCI Spooder-Man",
-        description: "Double rejection, a Steam-style lab meme, and a remix kit for joining the nerdy Spooder-Verse.",
+        description: "Double rejection, a Steam-style lab meme, and a small door into the nerdy Spooder-Verse.",
         section: "Projects",
         handler: () => {
           window.location.href = "/projects/hci-spooder-man/";
